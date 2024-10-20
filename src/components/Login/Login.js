c2759f6e778a0d710516426501b033475139828f
nbthinh <nguyenbuuthinh036@gmail.com>
2024-10-20 19:48:11 +0700

    #9.3.1

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,29 @@
+import "./Login.scss";
 const Login = (props) => {
     return (
-        <div className="login-container">
-            Login
-            <button className="btn btn-primary">Test bootstrap</button>
+        <div className="login-container mt-3">
+            <div className="container">
+                <div className="row">
+                    <div className="content-left col-7 red">
+                        <div className="brand">
+                            Hỏi dân IT
+                        </div>
+                        <div className="detail">
+                            Facebook helps you connect and share with the people in your life.
+                        </div>
+                    </div>
+                    <div className="content-right col-5 d-flex flex-column gap-3 py-3">
+                        <input type="text" className="form-control" placeholder="Email address or phone number" />
+                        <input type="password" className="form-control" placeholder="password" />
+                        <button className="btn btn-primary">Login</button>
+                        <span className="text-center">Forgot your password</span>
+                        <hr />
+                        <div className="text-center">
+                            <button className="btn btn-success">Create new Account</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
